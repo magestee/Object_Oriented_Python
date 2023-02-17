7bc90c569117829ce6345cfc10d3e189e68a8d10
Saman Gohari <mage@Samans-MacBook-Air.local>
2023-02-17 10:58:39 +0330

    :hammer: get student function improved

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
 def main():
-    name = get_name()
-    house = get_house()
+    name, house = get_student()
     print(f"{name} from {house}")
 
-def get_name():
-    return input("Name: ")
-
-def get_house():
-    return input("House: ")
+def get_student():
+    name = input("Name: ")
+    house = input("House: ")
+    return name, house
 
 if __name__ == "__main__":
     main()
