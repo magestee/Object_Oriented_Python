024b2e0c5bb0586e862328e7fd0cd69c67a186fb
Saman Gohari <mage@Samans-MacBook-Air.local>
2023-02-17 11:27:52 +0330

    :hammer: updated to object oriented

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
+class Student:
+    def __init__(self, name, house):
+        self.name = name
+        self.house = house
+
 def main():
     student = get_student()
-    print(f"{ student['name'] } from {student['house']}")
+    print(f"{ student.name } from {student.house}")
 
 def get_student():
-    student = {}
-    student["name"] = input("Name: ")
-    student["house"] = input("House: ")
+    name = input("Name: ")
+    house = input("House: ")
+    student = Student(name, house)
     return student
 
 if __name__ == "__main__":
