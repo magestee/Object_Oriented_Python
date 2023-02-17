89a96979b84c75e3d357a89cf50d6ae3797cc6af
Saman Gohari <mage@Samans-MacBook-Air.local>
2023-02-17 11:45:56 +0330

    :bookmark: version 1.00 completed

--- main.py
+++ main.py
@@ -1,11 +1,25 @@
 class Student:
     def __init__(self, name, house):
+        if not name:
+            raise ValueError("Missing Name")
+        if house not in ["Gryffindore", "Slytherin", "Hufflepuff", "Ravenclaw"]:
+            raise ValueError("Missing House")
+        
         self.name = name
         self.house = house
+    
+    def get_patronous(self):
+        if self.name == "Harry":
+            print("🦌")
+        elif self.name == "Ron":
+            print("🐕")
+        else:
+            print("🪄")
 
 def main():
     student = get_student()
     print(f"{ student.name } from {student.house}")
+    student.get_patronous()
 
 def get_student():
     name = input("Name: ")
